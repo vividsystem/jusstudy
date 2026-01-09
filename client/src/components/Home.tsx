67fa101e6cc90d62a5a03df10fa5b06005854814
vividsystem <notrealbutnotobot@gmail.com>
2026-01-09 13:03:53 +0100

    chore: remove bhvr boilerplate code

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,37 +1,6 @@
-import { useState } from "react";
-import { hcWithType } from "server/dist/client";
-import { useMutation } from "@tanstack/react-query";
 import { authClient } from "../lib/auth-client";
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000";
-
-
-const client = hcWithType(SERVER_URL);
-
-type ResponseType = Awaited<ReturnType<typeof client.hello.$get>>;
-
-
 function Home() {
-	const [data, setData] = useState<
-		Awaited<ReturnType<ResponseType["json"]>> | undefined
-	>();
-
-	const { mutate: sendRequest } = useMutation({
-		mutationFn: async () => {
-			try {
-				const res = await client.hello.$get();
-				if (!res.ok) {
-					console.log("Error fetching data");
-					return;
-				}
-				const data = await res.json();
-				setData(data);
-			} catch (error) {
-				console.log(error);
-			}
-		},
-	});
-
 	return (
 		<div className="max-w-xl mx-auto flex flex-col gap-6 items-center justify-center min-h-screen">
 			Jus'study
