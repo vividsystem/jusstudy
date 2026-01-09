e3b4f31f732f808abb4b6f55cf5452175396279b
vividsystem <notrealbutnotobot@gmail.com>
2026-01-09 13:34:03 +0100

    chore: fix typo in credentials for hackclub auth

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -2,11 +2,18 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { genericOAuth } from "better-auth/plugins";
 import db from "./db";
+import { accounts, sessions, users, verifications } from "./db/schema-auth";
 
-//default redirectUri: /oauth2/callback/hackclub-auth
+//default redirectUri: /api/auth/oauth2/callback/hackclub-auth
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: "pg",
+		schema: {
+			verifications,
+			users,
+			sessions,
+			accounts
+		},
 		usePlural: true
 	}),
 	trustedOrigins: [
@@ -18,7 +25,7 @@ export const auth = betterAuth({
 				{
 					providerId: "hackclub-auth",
 					clientId: process.env.HACKCLUB_AUTH_CLIENT_ID!,
-					clientSecret: process.env.HACkCLUB_AUTH_CLIENT_SECRET!,
+					clientSecret: process.env.HACKCLUB_AUTH_CLIENT_SECRET!,
 					discoveryUrl: "https://auth.hackclub.com/.well-known/openid-configuration",
 					scopes: ["openid", "profile", "email", "name", "slack_id", "verification_status"]
 				}
