60b0434cf8c0358943f76f275c424d4f49877534
vividsystem <notrealbutnotobot@gmail.com>
2026-01-17 14:50:44 +0100

    fix: type resolution for rpc hono client Type resolution for hono's rpc client only works when route declarations are chained apparently

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,51 +1,59 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { auth } from "./auth";
-import { users } from "./routes/users";
+import { usersRoutes } from "./routes/users";
 import { projectsRoute } from "./routes/projects";
+import type { hc } from "hono/client";
 
 const app = new Hono<{
 	Variables: {
 		user: typeof auth.$Infer.Session.user | null;
 		session: typeof auth.$Infer.Session.session | null
 	}
-}>().basePath("/api");
+}>().basePath("/api")
 
 
-app.use(
-	"/auth/*", // or replace with "*" to enable cors for all routes
-	cors({
-		origin: process.env.CORS_ORIGIN ?? "http://localhost:5173",
-		allowHeaders: ["Content-Type", "Authorization"],
-		allowMethods: ["POST", "GET", "OPTIONS"],
-		exposeHeaders: ["Content-Length"],
-		maxAge: 600,
-		credentials: true,
-	}),
-);
-app.use("*", async (c, next) => {
-	const session = await auth.api.getSession({ headers: c.req.raw.headers });
+	.use("*", cors({
+		origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+	}))
+	.use(
+		"/auth/*", // or replace with "*" to enable cors for all routes
+		cors({
+			origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+			allowHeaders: ["Content-Type", "Authorization"],
+			allowMethods: ["POST", "GET", "OPTIONS"],
+			exposeHeaders: ["Content-Length"],
+			maxAge: 600,
+			credentials: true,
+		}),
+	)
+	.use("*", async (c, next) => {
+		const session = await auth.api.getSession({ headers: c.req.raw.headers });
 
-	if (!session) {
-		c.set("user", null);
-		c.set("session", null);
+		if (!session) {
+			c.set("user", null);
+			c.set("session", null);
+			await next();
+			return;
+		}
+
+		c.set("user", session.user);
+		c.set("session", session.session);
 		await next();
-		return;
-	}
+	})
+	.on(["POST", "GET"], "/auth/*", (c) => {
+		return auth.handler(c.req.raw)
+	})
 
-	c.set("user", session.user);
-	c.set("session", session.session);
-	await next();
-});
-app.on(["POST", "GET"], "/auth/*", (c) => {
-	return auth.handler(c.req.raw)
-})
+	.get("/status", (c) => {
+		return c.json({ message: "Up and running!" })
+	})
 
-app.get("/status", (c) => {
-	return c.json({ message: "Up and running!" })
-})
+	.route("/projects", projectsRoute)
+	.route("/users", usersRoutes);
 
-app.route("/projects", projectsRoute)
-app.route("/users", users)
 
-export default app
+
+export type Client = ReturnType<typeof hc<AppType>>;
+export type AppType = typeof app;
+export default app;
