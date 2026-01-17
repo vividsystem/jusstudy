d84f4c2b3befff1e92a465a4cc36b1a6ce54e652
alexlam0206 <nok@pgmiv.com>
2026-01-17 16:38:15 +0800

    Correct layout, changed font

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -7,14 +7,14 @@ function Landing() {
 		<div className="relative inset-0 min-h-screen min-w-screen flex flex-col items-center justify-start">
 			<HackclubLogo />
 			<div className="relative mt-20 2xl:mt-0 min-h-screen flex flex-col justify-start w-4/6 2xl:gap-8 gap-4">
-				<h1 className="2xl:text-10xl/tight lg:text-9xl text-6xl font-serif font-bold max-h-fit">Jus'study</h1>
-				<p className="2xl:text-7xl lg:text-4xl text-lg font-serif">A Hack Club <span className="font-bold">You Ship, We Ship</span> where students build projects that help with studying, focus, learning, or school life, and earn prizes for shipping real work.</p>
-				<ul className="2xl:text-6xl lg:text-2xl text-md list-disc list-inside font-serif my-2 lg:my-10">
+				<h1 className="2xl:text-10xl/tight lg:text-9xl text-6xl font-bold max-h-fit title-font">Jus'study</h1>
+				<p className="2xl:text-7xl lg:text-4xl text-lg">A Hack Club <span>You Ship, We Ship</span> where students build projects that help with studying, focus, learning, or school life, and earn prizes for shipping real work.</p>
+				<ul className="2xl:text-6xl lg:text-2xl text-md list-disc list-inside my-2 lg:my-10">
 					<li>Build apps, tools, hardware, or experiments related to something you're studying</li>
 					<li>Upload your project with a short write-up</li>
 					<li>Earn prizes based on effort, creativity, and usefulness</li>
 				</ul>
-				<div className="2xl:text-5xl lg:text-3xl text-xl flex lg:flex-row flex-col gap-4 2xl:gap-12 lg:gap-8 items-center justify-center">
+				<div className="2xl:text-5xl lg:text-3xl text-xl flex lg:flex-row flex-col gap-6 2xl:gap-12 lg:gap-8 items-center justify-start">
 					<a
 						href="https://forms.fillout.com/t/aX86bHVxqkus"
 						target="_blank"
@@ -27,7 +27,7 @@ function Landing() {
 						href="/beginner"
 						className="bg-white text-black 2xl:px-20 2xl:py-8 lg:px-10 lg:py-4 px-5 py-2 rounded-lg border lg:border-2 2xl:border-3 border-black shadow-gray-700 shadow-lg flex items-center justify-center"
 					>
-						See beginner guides
+						See Beginner Guides
 					</a>
 				</div>
 			</div>
