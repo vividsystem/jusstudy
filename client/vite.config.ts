3f13dfc795f4fa8c51b7167b252a5ef7e559b56e
vividsystem <notrealbutnotobot@gmail.com>
2026-01-17 14:47:39 +0100

    fix: module resolution Monorepo stuff...

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@server': path.resolve(__dirname, '../server/src'),
+			'@client': path.resolve(__dirname, './src'),
+			'@shared': path.resolve(__dirname, '../shared/src'),
+		},
+	},
 });
