26e1397c3a0275534c22c14622b7fcd61c8e6e33
alexlam0206 <nok@pgmiv.com>
2026-01-18 22:55:16 +0800

    feat(ui): add colorbanner of the melting effect, edit layout and logic

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -16,10 +16,9 @@ export default function Button({
 	className = "",
 	target,
 	rel
-}: ButtonProps)
-
-{
-	const baseStyles = "px-4 py-2 lg:px-10 lg:py-4 2xl:px-12 2xl:py-6 rounded-[30px] shadow-lg flex items-center justify-center whitespace-nowrap transition-transform active:scale-95 transform-gpu backface-hidden";
+}: ButtonProps) {
+	const baseStyles = "px-4 py-2 lg:px-10 lg:py-4 2xl:px-12 2xl:py-6 rounded-[2rem] shadow-lg flex items-center justify-center whitespace-nowrap transition-transform active:scale-95 transform-gpu backface-hidden overflow-hidden";
+	
 	const variantStyles = variant === "yellow" 
 		? "bg-[#FFE6A7] text-[#6F1D1B] border-4 border-[#6F1D1B]" 
 		: "bg-[#6F1D1B] text-[#FFE6A7] border-4 border-[#FFE6A7]";
