ae1a732e336d7c73c42c1ca02c2fc59a983801cc
alexlam0206 <nok@pgmiv.com>
2026-01-18 11:01:19 +0800

    fix: scale problem

--- client/src/components/HackclubLogo.tsx
+++ client/src/components/HackclubLogo.tsx
@@ -1,7 +1,7 @@
 export default function HackclubLogo() {
 	return (
-		<a className="top-0 left-0 pl-8 fixed" href="https://hackclub.com">
-			<img src="/hackclub-flag-top.png" alt="Hackclub Logo" className="2xl:h-48 lg:h-24 h-20" />
+		<a className="top-0 left-0 pl-8 fixed" href="https://hackclub.com" tabIndex={-1}>
+			<img src="/hackclub-flag-top.png" alt="Hackclub Logo" className="2xl:h-32 lg:h-24 h-20" />
 		</a>
 	)
 }
