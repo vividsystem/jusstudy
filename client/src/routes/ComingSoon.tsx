4a9879183460d7f30e9c4e989b390cc6e67477ba
vividsystem <notrealbutnotobot@gmail.com>
2026-02-04 20:35:31 +0100

    feat: coming soon page (copium)

---
+++ client/src/routes/ComingSoon.tsx
@@ -0,0 +1,8 @@
+export default function ComingSoon() {
+	return (
+		<main className="w-full min-h-screen">
+			<h1 className="text-6xl"> This site is under construction and coming soon!</h1>
+			<img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExejI3YTZ1dW8zbnU4M282enI2NXR1Zjhlc29zN2Rlcnl2YTBtdGNvYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vR1dPIYzQmkRzLZk2w/giphy.gif" />
+		</main>
+	)
+}
