f43c597853d857deafbe3dc0ad84cdee48a228f8
vividsystem <notrealbutnotobot@gmail.com>
2026-02-23 20:19:56 +0100

    feat(ui): change navbar to show current route

--- client/src/routes/AppLayout.tsx
+++ client/src/routes/AppLayout.tsx
@@ -1,19 +1,35 @@
 import ErrorBoundary from "@client/components/ErrorBoundary";
 import UserIcon from "@client/components/UserIcon";
-import { Outlet } from "react-router";
+import { authClient } from "@client/lib/auth-client";
+import { Navigate, Outlet, useLocation } from "react-router";
+
+export function Link(props: { name: string, route: string, className: string }) {
+	const location = useLocation()
+	return (
+		<li className={`${props.className} ${location.pathname == props.route ? "bg-egg-yellow" : ""}`}><a href={props.route} className="text-6xl">{props.name}</a></li>
+	)
+}
+
 
 export default function AppLayout() {
+	const { data } = authClient.useSession()
+	if (data == null) {
+		return <Navigate to={"/"} />
+	}
 	return (
 		<ErrorBoundary>
 			<div className="flex flex-row">
 				<header className="w-1/4 2xl:w-1/6 flex flex-col justify-between h-screen p-4">
 					<ul className="list-none flex flex-col items-center gap-8">
-						<li className="rotate-5"><a href="/home" className="text-6xl">Home</a></li>
-						<li className="-rotate-5"><a href="/projects" className="text-6xl">Projects</a></li>
-						<li className="rotate-4"><a href="/explore" className="text-6xl">Explore</a></li>
-						<li className="rotate-4"><a href="/guides" className="text-6xl">Guides</a></li>
-						<li className="-rotate-5"><a href="/shop" className="text-6xl">Shop</a></li>
-						<li className=""><a href="/rankings" className="text-6xl">Rankings</a></li>
+						<Link className="rotate-5" route="/home" name="Home" />
+						<Link className="-rotate-5" route="/projects" name="Projects" />
+						<Link className="rotate-4" route="/explore" name="Explore" />
+						<Link className="rotate-4" route="/guides" name="Guides" />
+						<Link className="-rotate-5" route="/shop" name="Shop" />
+						<Link className="" route="/rankings" name="Rankings" />
+						{data.user.staff && (
+							<Link className="" route="/reviews" name="Reviews" />
+						)}
 					</ul>
 					<UserIcon />
 				</header>
