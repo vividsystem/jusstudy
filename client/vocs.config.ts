e802f8069076aa3455ba9bf29ce7ab393172ad49
Michael Robinson <mrobi343@eq.edu.au>
2026-07-12 08:41:55 +1000

    docs: edit config, added FAQ, add stationary organiser and add index

--- client/vocs.config.ts
+++ client/vocs.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 	sidebar: [
 		{
 			text: "Getting Started",
-			link: "/docs"
+			link: "/"
 		},
 		{
 			text: "Hardware",
@@ -17,11 +17,11 @@ export default defineConfig({
 			items: [
 				{
 					text: "Building a simple pomodoro timer",
-					link: "/docs/hw/first-project"
+					link: "/hw/first-project"
 				},
 				{
 					text: "Further Ressources",
-					link: "/docs/hw/ressources"
+					link: "/hw/ressources"
 				}
 			]
 		},
@@ -31,11 +31,49 @@ export default defineConfig({
 			items: [
 				{
 					text: "Building a simple to-do website",
-					link: "/docs/hw/first-project"
+					link: "/hw/first-project"
 				},
 				{
 					text: "Further Ressources",
-					link: "/docs/sw/ressources"
+					link: "/sw/ressources"
+				},
+					]
+				},
+		{
+			text: "Rewards",
+			collapsed: false,
+			items: [
+				{
+					text: "How it works",
+					link: "/rewards/first-project"
+				},
+				{
+					text: "Further Ressources",
+					link: "/rewards/ressources"
+				}
+			]
+		},
+		{
+			text: "Guides",
+			collapsed: false,
+			items: [
+				{
+					text: "Make a Stationary Organiser",
+					link: "/guides/stationary-organiser"
+				}
+			]
+		},
+		{
+			text: "Help",
+			collapsed: false,
+			items: [
+				{
+					text: "Resources",
+					link: "/help/resources"
+				},
+				{
+					text: "FAQ",
+					link: "/help/faq"
 				}
 			]
 		}
