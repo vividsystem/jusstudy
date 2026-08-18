a405e82564d9fdf947d7129fabee42bd11e736b1
vividsystem <notrealbutnotobot@gmail.com>
2026-08-18 19:54:14 +0200

    style(client): change button corner radius to `2xl`

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -14,7 +14,7 @@ type ButtonProps = {
 } & BaseParentProps;
 
 export default function Button(props: ButtonProps) {
-	const baseStyles = "px-4 py-2 lg:px-10 lg:py-4 2xl:px-12 2xl:py-6 rounded-4xl shadow-lg flex items-center justify-center whitespace-nowrap transition-transform active:scale-95 transform-gpu backface-hidden overflow-hidden";
+	const baseStyles = "px-4 py-2 lg:px-10 lg:py-4 2xl:px-12 2xl:py-6 rounded-2xl flex items-center justify-center whitespace-nowrap transition-transform active:scale-95 transform-gpu backface-hidden overflow-hidden";
 
 	if ("href" in props) {
 		return (
